3b5ec7d20ad91e23aea0fc61d836355b33d21955
yejichoi <chldpwl5620@gmail.com>
2023-10-29 23:27:31 +0900

    add: django_tuieditor 추가

--- app/api/models.py
+++ app/api/models.py
@@ -7,7 +7,7 @@
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 from app.core.models import TimestampedModel
-# from django_tuieditor.models import MarkdownField
+from django_tuieditor.models import MarkdownField
 
 class Content(TimestampedModel):
     # Field name made lowercase.
@@ -40,12 +40,8 @@ class ContentsDetail(Content):
     # Field name made lowercase.
     ctnt_dtl_inp_user = models.CharField(
         db_column='INP_USER', max_length=30, blank=True, null=True, db_comment='작성자')
-    # Field name made lowercase.
-    # ctnt_body = django_tuieditor.MarkdownField(rendered_field='text_rendered', use_editor=False, use_admin_editor=False)
+    ctnt_body = MarkdownField(blank=True, null=True)
         # db_column='CTNT_BODY', max_length=5000, blank=True, null=True, db_comment='콘텐츠 내용')
-    # Field name made lowercase.
-    ctnt_body = models.TextField(
-        db_column='CTNT_BODY', max_length=5000, blank=True, null=True, db_comment='콘텐츠 내용')
     # Field name made lowercase.
     ctnt_path = models.TextField(
         db_column='CTNT_PATH', max_length=200, blank=True, null=True, db_comment='콘텐츠 파일 경로')
